93a48a74baa59756f477447d8aeba147b4647537
venkatesh <venkigvengadesh@gmail.com>
2025-01-04 15:09:23 +0530

    Added necessary dependencies and fixed build errors

--- backend/src/test/java/AppTest.java
+++ backend/src/test/java/AppTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 public class AppTest {
     @Test
     public void testApp() {
-        assertTrue(true);
+        assertTrue(true);  // This is just a simple test
     }
 }
 
